2d1ea872381efe8f823e9a5fea535b1b521d37c2
Karen Barnert <sakura.emina@gmail.com>
2014-06-26 17:37:11 -0400

    Busqueda de partes por rut sin login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	 @tickets = Ticket.where('name = ?', "%#{params[:search]}%")
+  	 @tickets = Ticket.where('rut = ?', "#{params[:search]}")
   end
 end
 
